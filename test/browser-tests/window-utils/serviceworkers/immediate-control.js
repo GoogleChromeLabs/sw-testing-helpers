f4544a8f8c7b045cefc4a2b3c689eefc4937598e
Jeff Posnick <jeffy@google.com>
2017-03-24 14:17:29 -0400

    Added a comment.

--- test/browser-tests/window-utils/serviceworkers/immediate-control.js
+++ test/browser-tests/window-utils/serviceworkers/immediate-control.js
@@ -1,2 +1,5 @@
+// This is used by the controlled-by-sw.js test.
+// It's a service worker that activates & takes control of clients immediately.
+
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', () => self.clients.claim());
